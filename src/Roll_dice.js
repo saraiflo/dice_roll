896eb6329d76d1fa2f94dadb55a30b91c748815a
Sarai <saraiflores@gmail.com>
2019-05-27 11:27:34 -0400

    styling code update

--- src/Roll_dice.js
+++ src/Roll_dice.js
@@ -9,7 +9,7 @@ class RollDice extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { die1: "one", die2: "one" };
+    this.state = { die1: "one", die2: "one", rolling: false };
     this.roll = this.roll.bind(this);
   }
 
@@ -21,17 +21,25 @@ class RollDice extends Component {
     const rolled2 = this.props.sides[
       Math.floor(Math.random() * this.props.sides.length)
     ];
-    this.setState({ die1: rolled1, die2: rolled2 });
-  }
+    //updating state of the dice and the state of rolling
+    this.setState({ die1: rolled1, die2: rolled2, rolling: true });
 
+    //wait, once second then resetting state to false
+    setTimeout(() => {
+      this.setState({ rolling: false });
+    }, 1000);
+  }
   render() {
     return (
       <div className="Rolldice">
         <div className="Rolldice-container">
-          <Die face={this.state.die1} />
-          <Die face={this.state.die2} />
+          <Die face={this.state.die1} rolling={this.state.rolling} />
+          <Die face={this.state.die2} rolling={this.state.rolling} />
         </div>
-        <button onClick={this.roll}> Click to Roll!</button>
+        <button onClick={this.roll} disabled={this.state.rolling}>
+          {" "}
+          {this.state.rolling ? "Rolling...." : "Click to Roll!"}
+        </button>
       </div>
     );
   }
